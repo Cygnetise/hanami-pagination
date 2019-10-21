eccf78997c8605509287710861a7bf76645e616e
DangerDawson <david.dawson@gmail.com>
2019-10-21 12:25:34 +0100

    Namespace the pager for the view

--- lib/hanami/pagination/view.rb
+++ lib/hanami/pagination/view.rb
@@ -5,79 +5,104 @@ module Hanami
     module View
       include Hanami::Helpers
 
-      def next_page_url
-        page_url(pager.next_page)
+      def self.[](*prefixes)
+        Base.new(prefixes)
       end
 
-      def prev_page_url
-        page_url(pager.prev_page)
-      end
-
-      def page_url(page)
-        "#{params.env['REQUEST_PATH']}?page=#{page}"
-      end
-
-      def previous_page_path(page)
-        routes.path(page, **params, page: pager.prev_page)
-      end
-
-      def next_page_path(page)
-        routes.path(page, **params, page: pager.next_page)
-      end
-
-      def n_page_path(page, n)
-        routes.path(page, **params, page: n)
-      end
-
-      def paginate(page)
-        html.nav(class: 'pagination') do
-          content = []
-
-          content << first_page_tag(page) unless pager.first_page?
-          content << ellipsis_tag if pager.current_page > 3
-          content << previous_page_tag(page) if pager.current_page > 2
-          content << current_page_tag
-          content << next_page_tag(page) if (pager.total_pages - pager.current_page) > 1
-          content << ellipsis_tag if (pager.total_pages - pager.current_page) > 3
-          content << last_page_tag(page) unless pager.last_page?
-
-          raw(content.map(&:to_s).join)
+      class Base < Module
+        def initialize(prefixes = [nil])
+          @prefixes = prefixes
         end
-      end
 
-      def first_page_tag(page)
-        html.a(href: n_page_path(page, 1), class: 'pagination-first-page') do
-          '1'
+        def included
+          prefixes = @prefixes
+
+          prefixes.each do |prefix|
+            prefixer = proc do |string|
+              [prefix, string].compact.join('_').to_sym
+            end
+
+            define_method(prefixer.call(:next_page_url)) do
+              send(prefixer.call(:page_url), send(prefixer.call(:pager), :next_page))
+            end
+
+            define_method(prefixer.call(:prev_page_url)) do
+              send(prefixer.call(:page_url), send(prefixer.call(:pager), :prev_page))
+            end
+
+            define_method(prefixer.call(:page_url)) do |page|
+              "#{params.env['REQUEST_PATH']}?#{prefixer.call(:page)}=#{page}"
+            end
+
+            define_method(prefixer.call(:previous_page_path)) do |page|
+              routes.path(page, **params, prefixer(:page) => send(prefixer.call(:pager), :prev_page))
+            end
+
+            define_method(prefixer.call(:next_page_path)) do |page|
+              routes.path(page, **params, prefixer(:page) => send(prefixer.call(:pager), :next_page))
+            end
+
+            define_method(prefixer.call(:n_page_path)) do |page, n|
+              routes.path(page, **params, prefixer(:page) => n)
+            end
+
+            define_method(prefixer.call(:paginate)) do |page|
+              html.nav(class: 'pagination') do
+                pager = send(prefixer.call(:pager))
+                content = []
+
+                content << send(prefixer.call(:first_page_tag), page) unless pager.first_page?
+                content << send(prefixer.call(:ellipsis_tag)) if pager.current_page > 3
+                content << send(prefixer.call(:previous_page_tag), page) if pager.current_page > 2
+                content << send(prefixer.call(:current_page_tag))
+                content << send(prefixer.call(:next_page_tag), page) if (pager.total_pages - pager.current_page) > 1
+                content << send(prefixer.call(:ellipsis_tag)) if (pager.total_pages - pager.current_page) > 3
+                content << send(prefixer.call(:last_page_tag), page) unless pager.last_page?
+
+                raw(content.map(&:to_s).join)
+              end
+            end
+
+            define_method(prefixer.call(:first_page_tag)) do |page|
+              html.a(href: send(prefixer.call(:n_page_path), page, 1), class: 'pagination-first-page') do
+                '1'
+              end
+            end
+
+            define_method(prefixer.call(:previous_page_tag)) do |page|
+              html.a(href: send(prefixer.call(:previous_page_path), page), class: 'pagination-previous-page') do
+                pager = send(prefixer.call(:pager))
+                pager.prev_page
+              end
+            end
+
+            define_method(prefixer.call(:current_page_tag)) do
+              html.span(class: 'pagination-current-page') do
+                pager = send(prefixer.call(:pager))
+                pager.current_page
+              end
+            end
+
+            define_method(prefixer.call(:last_page_tag)) do |page|
+              total_pages = pager.total_pages
+              html.a(href: send(prefixer.call(:n_page_path), page, total_pages), class: 'pagination-last-page') do
+                total_pages
+              end
+            end
+
+            define_method(prefixer.call(:next_page_tag)) do |page|
+              pager = send(prefixer.call(:pager))
+              html.a(href: send(prefixer.call(:next_page_path), page), class: 'pagination-next-page') do
+                pager.next_page
+              end
+            end
+          end
         end
-      end
 
-      def previous_page_tag(page)
-        html.a(href: previous_page_path(page), class: 'pagination-previous-page') do
-          pager.prev_page
-        end
-      end
-
-      def current_page_tag
-        html.span(class: 'pagination-current-page') do
-          pager.current_page
-        end
-      end
-
-      def last_page_tag(page)
-        html.a(href: n_page_path(page, pager.total_pages), class: 'pagination-last-page') do
-          pager.total_pages
-        end
-      end
-
-      def next_page_tag(page)
-        html.a(href: next_page_path(page), class: 'pagination-next-page') do
-          pager.next_page
-        end
-      end
-
-      def ellipsis_tag
-        html.span(class: 'pagination-ellipsis') do
-          '...'
+        def ellipsis_tag
+          html.span(class: 'pagination-ellipsis') do
+            '...'
+          end
         end
       end
     end

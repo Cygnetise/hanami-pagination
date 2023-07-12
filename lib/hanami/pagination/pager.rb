5a3f770c33671b73dbb1a8ca12c05b9c5cbe2773
David Dawson <david.dawson@gmail.com>
2023-07-12 16:43:21 +0100

    Optimise the pager to remove N+1 queries

--- lib/hanami/pagination/pager.rb
+++ lib/hanami/pagination/pager.rb
@@ -8,11 +8,12 @@ module Hanami
       end
 
       def next_page
-        pager.next_page
+        num = current_page + 1
+        num if total_pages >= num
       end
 
       def prev_page
-        pager.prev_page
+        @prev_page ||= pager.prev_page
       end
 
       def total
@@ -20,37 +21,37 @@ module Hanami
       end
 
       def total_pages
-        pager.total_pages
+        @total_pages ||= (total / pager.per_page.to_f).ceil
       end
 
       def current_page
-        pager.current_page
+        @current_page ||= pager.current_page
       end
 
       def current_page?(page)
-        pager.current_page == page
+        current_page == page
       end
 
       def pages_range(delta: 3)
-        first = pager.current_page - delta
+        first = current_page - delta
         first = first > 0 ? first : 1
 
-        last = pager.current_page + delta
-        last = last < pager.total_pages ? last : pager.total_pages
+        last = current_page + delta
+        last = last < total_pages ? last : total_pages
 
         (first..last).to_a
       end
 
       def all_pages
-        (1..pager.total_pages).to_a
+        (1..total_pages).to_a
       end
 
       def first_page?
-        pager.current_page == 1
+        current_page == 1
       end
 
       def last_page?
-        pager.total < 1 || pager.current_page == pager.total_pages
+        total < 1 || current_page == total_pages
       end
     end
   end
